0501ea60c71d587f09091bebffcb9eb3afbeb0ee
Nathaniel Tan <nathanieltan.marlowen.498@gmail.com>
2022-06-11 22:11:21 +1000

    We'll see

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -3,6 +3,7 @@
 
 
 from concurrent.futures import ThreadPoolExecutor
+from tkinter.messagebox import NO
 
 
 def is_odd(a_number):
@@ -24,7 +25,7 @@ def is_odd(a_number):
 
 
 def fix_it(moves=True, should_move=True):
-    if moves == True:
+    if moves == NO:
         return should_move
     if should_move == True:
         return "No Problem"
@@ -67,7 +68,10 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    list = []
+    for i in range(10):
+        list.append("*")
+    return list
 
 
 def loops_1c(number_of_items=5, symbol="#"):
