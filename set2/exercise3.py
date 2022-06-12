300db3abe3588d7b60d1702e20327f377616e89a
Nathaniel Tan <nathanieltan.marlowen.498@gmail.com>
2022-06-12 13:12:02 +1000

    we'll see

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -140,7 +140,7 @@ def loops_3():
     for i in range(10):
         row_list = []
         for j in range(10):
-         row_list.append(i)
+         row_list.append(str(i))
         number_list.append(row_list)
     return number_list
 
